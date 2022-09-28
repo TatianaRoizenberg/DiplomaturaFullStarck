6432bf65a4c6e176c9353f5ae8be55e79e3fccf0
Tatiana Roizenberg <tatiroizenberg@gmail.com>
2022-09-27 21:01:36 -0300

    JavaScrip Intro

---
+++ clase-10/js/mail.js
@@ -0,0 +1,45 @@
+// clase 10 intro js
+
+console.log("hola mundo desde archivo externo");
+
+// SI PONDO COMILLLAS NO ES NUMERO (CONSTANTE Y SE HACE CUENTAS)
+// SI PONDO COMILLAS ES TEXTO (VARIABLE )
+// LET ES LA FUNCION ACTUAL 
+// VAR ES LA FUNCION ANTERIOR 
+// strink=cadena de caracteres
+// prompt=sumas y bla
+// W3school nos da guia
+
+
+// Ejercicio 1 de js
+let nombre="Tatiana";
+let apellido="Roi";
+let edad=22;
+let mascota=false;
+
+console.log(nombre)
+console.log(apellido)
+console.log(edad)
+console.log(mascota)
+// sin comillas patra que me muestre lo que guarde  ejemplo: console.log(nombre)
+// con comillas para que me mueste el texto 
+
+
+// operaciones 
+let numeroA= 12 
+let numeroB= 13
+let numeroC= 14
+
+console.log (edad)
+let resultadoSuma= (numeroA + numeroB + numeroC);
+console.log (resultadoSuma);
+
+
+// Concatenar el texto
+
+let textoaA ="HOLA"
+let textoB="Mundo"
+
+console.log(textoaA+textoB);
+console.log (resultadoA);
+
